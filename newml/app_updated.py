c0e1eaf29d790606574c893127e7964db505e298
Subham Mohanty <subhamomm836@gmail.com>
2025-06-01 17:49:27 +0530

    Update app_updated.py

--- newml/app_updated.py
+++ newml/app_updated.py
@@ -235,4 +235,5 @@ async def voice_input(file: UploadFile = File(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app_updated:app", host="0.0.0.0", port=8080, reload=True)
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run("app_updated:app", host="0.0.0.0", port=port)
